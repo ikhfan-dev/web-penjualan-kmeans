d6a69333cf2f8253e10e4edb6b1c84017bcd1591
Ikhfan Dev <ikhfan.dev@gmail.com>
2026-01-19 10:20:50 +0700

    update for deploy render

--- config.py
+++ config.py
@@ -6,7 +6,13 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
+    # Render PostgreSQL Fix
+    uri = os.environ.get('DATABASE_URL')
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(hours=2)
     
